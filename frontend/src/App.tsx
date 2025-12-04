5e3b17aeef17a076d6003109a3eaceecbf299d2e
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2025-12-04 14:50:56 +0530

    Initial Docker Setup: FE + BE + DB

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export const App: React.FC = () => {
+    return (
+        <div style={{ fontFamily: "sans-serif", padding: "1.5rem" }}>
+            <h1>EkLine Document App (Skeletons)</h1>
+        </div>
+    );
+};
