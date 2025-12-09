5782faf562774d619e55305ca9ca327a1c7441d9
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2025-12-09 16:21:20 +0530

    Fixed FE Port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3000,
+        port: 5000,
         host: "0.0.0.0",
         allowedHosts: ["ablecredit.deepakpriyadarshi.com", "localhost"],
     },
